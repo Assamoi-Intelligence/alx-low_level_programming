e4f3301cf88e641e48a44b64720ae3f07f11de1a
Assamoi-Intelligence <juniasdaniel05@gmail.com>
2022-03-16 09:00:25 -0700

    update betty format of code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
-int main(void) {
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
 	printf("Size of a char: %d byte(s)", sizeof(char));
 	printf("Size of a int: %d byte(s)", sizeof(int));
 	printf("Size of a long int: %d byte(s)", sizeof(long int));

35ec60629ed9e92bc73ed2dcedc2455ced2360f5
Assamoi-Intelligence <juniasdaniel05@gmail.com>
2022-03-16 09:12:59 -0700

    update task 4 and 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,8 @@
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
